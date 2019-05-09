e358f938fe0ee0cee7deb33fe3ddfcf334c35f1e
Douglas Hill <dohill06@gmail.com>
2019-05-09 12:41:46 -0700

    Set fetch API

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,23 @@ import apiKey from '../config';
 
 class App extends Component {
 
+  state = {
+    searchPic: []
+  };
+
+  componentDidMount() {
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=sunsets&per_page=24&format=json&nojsoncallback=1`)
+      .then(res => res.json())
+      .then(resData => {
+        this.setState({searchPic: resData.photos.photo});
+      })
+      .catch(err => {
+        console.log('Error fetching and parsing data', err);
+      }); 
+  }
+
   render() {
+    console.log(this.state.searchPic);
     return (
       <BrowserRouter>
         <div className="container">          
