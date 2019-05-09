74c171fd700c9403b76bb6321316ada971409168
Douglas Hill <dohill06@gmail.com>
2019-05-09 08:08:07 -0700

    Set up components folder and Header.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 
 import apiKey from './config';
 
 
-const Header = () => {
-  return (
-    <header>
-      <Form />
 
-      <Nav />
-    </header>
-  );
-}
 
 const Nav = () => {
   return (
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <Form />
+
+      <Nav />
+    </header>
+  );
+}
+
+export default Header;
