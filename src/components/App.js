96c7ff306361eec8bd390e9614023986a4f48b48
Douglas Hill <dohill06@gmail.com>
2019-05-13 15:27:29 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -19,23 +19,46 @@ class App extends Component {
 
   state = {
     searchPic: [],
+    catPic: [],
+    dogPic: [],
+    compPic: [],
     query: '',
     loading: true
   };
 
   componentDidMount() {
     this.searchFunc();
+    this.searchFunc('cats');
+    this.searchFunc('dogs');
+    this.searchFunc('computers');
   }
 
   searchFunc = (query = 'trees') => {
     fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
       .then(res => res.json())
       .then(resData => {
-        this.setState({
-          searchPic: resData.photos.photo,
-          query: `${query}`,
-          loading: false
-        });
+        if (query === 'cats') {
+          this.setState({
+            catPic: resData.photos.photo,
+            loading: false
+          });
+        } else if (query === 'dogs') {
+          this.setState({
+            dogPic: resData.photos.photo,
+            loading: false
+          });
+        } else if (query === 'computers') {
+          this.setState({
+            compPic: resData.photos.photo,
+            loading: false
+          });
+        } else {
+          this.setState({
+            searchPic: resData.photos.photo,
+            query: `${query}`,
+            loading: false
+          });
+        }
       })
       .catch(err => {
         console.log('Error fetching and parsing data', err);
@@ -57,17 +80,17 @@ class App extends Component {
                   
                   <Route path="/cats" render={ () => <React.Fragment>
                     <Header onSearch={this.searchFunc('cats')} />
-                    <Gallery data={this.state.searchPic} query={this.state.query} />
+                    <Gallery data={this.state.catPic} query="cats" />
                   </React.Fragment> } />
 
                   <Route path="/dogs" render={ () => <React.Fragment>
                     <Header onSearch={this.searchFunc('dogs')} />
-                    <Gallery data={this.state.searchPic} query={this.state.query} />
+                    <Gallery data={this.state.dogPic} query="dogs" />
                   </React.Fragment> } /> 
                   
                   <Route path="/computers" render={ () => <React.Fragment>
                     <Header onSearch={this.searchFunc('computers')} />
-                    <Gallery data={this.state.searchPic} query={this.state.query} />
+                    <Gallery data={this.state.compPic} query="computers" />
                   </React.Fragment> } />
               </Switch>
           }         
