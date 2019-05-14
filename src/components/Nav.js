c1df61dcc7f32cc10e14015e9a8624e4e3492847
Douglas Hill <dohill06@gmail.com>
2019-05-13 18:41:44 -0700

    Add Home link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,6 +5,7 @@ const Nav = () => {
   return (
     <nav className="main-nav">
       <ul>
+        <li><NavLink exact to='/'>Home</NavLink></li>
         <li><NavLink to='/cats'>Cats</NavLink></li>
         <li><NavLink to='/dogs'>Dogs</NavLink></li>
         <li><NavLink to='/computers'>Computers</NavLink></li>
